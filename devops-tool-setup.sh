f8aa78a3b214f798c5766c8ec685fa7534e7a1ae
pks-devops <57346143+pk-1986@users.noreply.github.com>
2025-07-21 00:27:14 +0530

    Update devops-tool-setup.sh

--- devops-tool-setup.sh
+++ devops-tool-setup.sh
@@ -81,5 +81,26 @@ if [ $var -ne 0 ]
    else
     echo "psql install ok"
   fi
-  
+echo ########################################
+# for ARM systems, set ARCH to: `arm64`, `armv6` or `armv7`
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+
+# (Optional) Verify checksum
+curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
+
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+
+sudo install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
+
+echo #####################################################################
+
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+kubectl version --client
+
+echo ############################################################3
    
