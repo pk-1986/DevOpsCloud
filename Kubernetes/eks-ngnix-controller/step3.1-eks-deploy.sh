64542851c7a3e0de3edacfcfe5024c12cd8ba599
pks-devops <57346143+pk-1986@users.noreply.github.com>
2025-07-26 20:23:17 +0530

    Update step3.1-eks-deploy.sh

--- Kubernetes/eks-ngnix-controller/step3.1-eks-deploy.sh
+++ Kubernetes/eks-ngnix-controller/step3.1-eks-deploy.sh
@@ -2,7 +2,7 @@
 aws eks --region us-west-2 update-kubeconfig --name $1
 kubectl get nodes
 kubectl create namespace prod
-kubectl apply -f nginx-deployment.yaml
-kubectl apply -f nginx-service.yaml
+kubectl apply -f step2-eks-deployment.yaml
+kubectl apply -f step3-eks-service.yaml
 # Get external LoadBalancer IP
 kubectl get svc 
