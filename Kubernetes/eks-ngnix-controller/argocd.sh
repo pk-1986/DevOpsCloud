2da6a3cfe45a7c811dcfde621e4fd7a0a71f5159
pks-devops <57346143+pk-1986@users.noreply.github.com>
2025-07-30 00:47:24 +0530

    Update argocd.sh

--- Kubernetes/eks-ngnix-controller/argocd.sh
+++ Kubernetes/eks-ngnix-controller/argocd.sh
@@ -17,3 +17,14 @@ argocd app create myapp \
   --dest-server https://kubernetes.default.svc \
   --dest-namespace default
 
+============================================
+
+# Download the latest version
+VERSION=$(curl -s https://api.github.com/repos/argoproj/argo-cd/releases/latest | grep tag_name | cut -d '"' -f 4)
+curl -sSL -o argocd "https://github.com/argoproj/argo-cd/releases/download/${VERSION}/argocd-linux-amd64"
+
+# Make it executable
+chmod +x argocd
+
+# Move it to your PATH
+sudo mv argocd /usr/local/bin/
