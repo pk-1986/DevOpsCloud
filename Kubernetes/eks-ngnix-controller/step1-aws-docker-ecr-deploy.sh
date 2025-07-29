35a2a8db0d7db34389388c4f62afe77601f6a827
pks-devops <57346143+pk-1986@users.noreply.github.com>
2025-07-30 00:17:28 +0530

    Update step1-aws-docker-ecr-deploy.sh

--- Kubernetes/eks-ngnix-controller/step1-aws-docker-ecr-deploy.sh
+++ Kubernetes/eks-ngnix-controller/step1-aws-docker-ecr-deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 echo "Please update AWS configue ######################################"
+sudo chmod 777 /var/run/docker.sock
 ecrUri="590183727102.dkr.ecr.us-west-2.amazonaws.com/prd/tom80"
 url=$(echo $ecrUri  | cut -d "/" -f1)
 # 1. Authenticate Docker to ECR
-aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 590183727102.dkr.ecr.us-west-2.amazonaws.com
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin $url
 
 # 2. Create ECR repository (only once)
 #aws ecr create-repository --repository-name nginx-demo
