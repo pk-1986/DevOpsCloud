f357cc4014e9d2afe2245c94a4349126ff7ac9a3
pks-devops <57346143+pk-1986@users.noreply.github.com>
2025-09-22 11:36:32 +0530

    Update deploy.sh

--- CloudFormation/deploy.sh
+++ CloudFormation/deploy.sh
@@ -1,5 +1,5 @@
 aws cloudformation create-stack \
   --stack-name ec2-vpc-stack \
   --template-body file://ec2-vpc.yml \
-  --parameters ParameterKey=KeyName,ParameterValue=your-keypair-name \
+  --parameters ParameterKey=KeyName,ParameterValue=my-ec2-key \
   --capabilities CAPABILITY_NAMED_IAM
