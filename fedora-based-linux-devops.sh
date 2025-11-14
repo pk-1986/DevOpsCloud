26bcda1fcb40d228c502f820d81157bf5e16611c
pks-devops <57346143+pk-1986@users.noreply.github.com>
2025-11-14 20:45:48 +0530

    Update fedora-based-linux-devops.sh

--- fedora-based-linux-devops.sh
+++ fedora-based-linux-devops.sh
@@ -1,3 +1,6 @@
+https://www.jenkins.io/doc/book/installing/linux/#fedora
+
+
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
