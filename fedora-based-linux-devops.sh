4e5d060852b6ad81cea5edc8bd2d3ca2c4e8e043
pks-devops <57346143+pk-1986@users.noreply.github.com>
2025-11-14 20:35:33 +0530

    Update fedora-based-linux-devops.sh

--- fedora-based-linux-devops.sh
+++ fedora-based-linux-devops.sh
@@ -3,6 +3,12 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo \
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo dnf upgrade -y
 # Add required dependencies for the jenkins package
-sudo dnf install fontconfig java-21-openjdk -y
+sudo dnf install fontconfig java-17-openjdk -y
 sudo dnf install jenkins -y
 sudo systemctl daemon-reload
+
+
+sudo systemctl enable jenkins
+
+sudo systemctl start jenkins
+
